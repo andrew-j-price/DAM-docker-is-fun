7c1d1a79f3b2df97d34727230b2a39e4c6a4bf5a
andrpric <andrpric@cisco.com>
2016-01-06 20:57:14 -0500

    my comments here

---
+++ run.sh
@@ -0,0 +1,21 @@
+# USE AT YOUR OWN RISK
+# This will stop, delete, and create some fresh running containers
+
+echo '# docker ps -a'
+docker ps -a
+
+echo ''
+echo '# stop running containers'
+docker stop `docker ps --no-trunc -q`
+
+echo ''
+echo '# delete all containers'
+docker rm `docker ps --no-trunc -aq`
+
+echo ''
+echo '# starting up fresh containers'
+docker run -d -p 2822:2812 -p 127.0.0.1:222:22 ubuntu-monit-ansible
+
+echo ''
+echo '# docker ps -a'
+docker ps -a
