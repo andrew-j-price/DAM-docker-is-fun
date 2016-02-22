29b9326d5360b8f99da78fa1539911ad60bc5f06
andrpric <andrpric@cisco.com>
2016-02-22 00:23:53 -0500

    my comments here

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ docker build --tag="base-centos-7" ./base-images/base-centos-7/
 docker build --tag="base-ubuntu-14.04" ./base-images/base-ubuntu-14.04/
 
 # Build the images the containers will run from
+docker build --tag="centos-monit-ansible" ./examples/centos-monit-ansible/
 docker build --tag="ubuntu-monit-ansible" ./examples/ubuntu-monit-ansible/
